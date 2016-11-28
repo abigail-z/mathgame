76a6bd517e8757d6514c33997d92153da758e4d5
Nathaniel Zeeman <nathanielzeeman@gmail.com>
2016-11-28 14:30:00 -0800

    test commit

---
+++ index.php
@@ -0,0 +1,6 @@
+<!DOCTYPE html>
+<html lang="en">
+<body>
+<?php echo $_SESSION['login_user']; ?>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+
+$_SESSION['login_user'] = 3;
+?>
+<!DOCTYPE html>
+<html lang="en">
+<body>
+<a href="index.php">go</a>
+</body>
+</html>

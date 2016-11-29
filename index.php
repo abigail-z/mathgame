3d43bad5d6b492e0ed14727131a6d6e58587f6ee
Nathaniel Zeeman <nathanielzeeman@gmail.com>
2016-11-28 17:51:02 -0800

    first commit

--- index.php
+++ index.php
@@ -4,6 +4,11 @@ if (!isset($_SESSION['login_user'])) {
 	header("Location: http://a00999093.azurewebsites.net/login.php");
 	die();
 }
+if ($_SESSION['login_user'] != "a@a.a" && $_SESSION['password_user'] != "aaa") {
+	header("Location: http://a00999093.azurewebsites.net/login.php");
+	die();
+	$_SESSION['login_error'] = "Invalid credentials";
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
--- login.php
+++ login.php
@@ -1,11 +1,17 @@
 <?php
 session_start();
 
-# $_SESSION['login_user'] = 3;
+$_SESSION['login_user'] = "a@a.a";
+$_SESSION['password_user'] = "aaa";
+# todo:
+# create a form and ue it to post username and password
+# create a new page to authenticate info and redirect as needed
 ?>
 <!DOCTYPE html>
 <html lang="en">
 	<body>
-		<a href="index.php">go</a>
+		<form action="http://a00999093.azurewebsites.net/authentication.php" method="post">
+			<input type="email" name="email" size="6" placeholder="Enter email" />
+		</form>
 	</body>
 </html>

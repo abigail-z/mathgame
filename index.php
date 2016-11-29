e8429a7bf461969727c7e99f39b08fec94e297a6
Nathaniel Zeeman <nathanielzeeman@gmail.com>
2016-11-28 18:44:00 -0800

    added scoring system, maybe

--- index.php
+++ index.php
@@ -5,9 +5,16 @@ if (!isset($_SESSION['user'])) {
 	die();
 }
 
+if(isset($_POST['answer'])) {
+	if ($_POST['answer'] == $_SESSION['total']) {
+		$_SESSION['correct'] += 1;
+	}
+	$_SESSION['answered'] += 1;
+}
+
 $number1 = rand(0,20);
 $number2 = rand(0,20);
-$total = $number1 + $number2;
+$_SESSION['total'] = $number1 + $number2;
 
 ?>
 <!DOCTYPE html>
@@ -18,5 +25,6 @@ $total = $number1 + $number2;
 			<input type="text" name="answer" placeholder="Enter answer" />
 			<input type="submit" value="Submit" />
 		</form>
+	<p><?php echo $_SESSION['correct'] . "/" . $_SESSION['answered'] ?></p>
 	</body>
 </html>

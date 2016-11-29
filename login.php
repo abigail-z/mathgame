3d41c1f65c7722e5211e6ca339aa42a79080613a
Nathaniel Zeeman <nathanielzeeman@gmail.com>
2016-11-28 17:55:31 -0800

    added some style to the login

--- login.php
+++ login.php
@@ -10,9 +10,12 @@ $_SESSION['password_user'] = "aaa";
 <!DOCTYPE html>
 <html lang="en">
 	<body>
+		<h1>Log in</h1>
 		<form action="http://a00999093.azurewebsites.net/authentication.php" method="post">
 			<input type="text" name="email" size="20" placeholder="email" />
+			<br/>
 			<input type="password" name="email" size="20" placeholder="password" />
+			<br/>
 			<input type="submit" value="Log in" />
 		</form>
 	</body>
